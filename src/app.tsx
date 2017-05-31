04dcccbb884498c555945b553479546a41ea7f19
Dmitry9 <Karpov.Dmitry80@gmail.com>
2017-05-31 20:56:42 +0300

    added mediaquery

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,25 @@
-import { style } from 'typestyle';
+import { style, media } from 'typestyle';
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-const className = style({
-	color: 'red'
-})
+// const fontSize = (value: number | string) => {
+// 	const valueStr = typeof value === 'string'
+// 		? value
+// 		: value + 'px'
+// 	return {
+// 		fontSize: valueStr
+// 	}
+// }
+
+const className = style(
+		//fontSize('2em'),
+		{ 
+			color: 'red',
+			transition: 'font-size .2s', 
+		},
+		media({ minWidth: 500, maxWidth: 700 }, { fontSize: '30px' }),
+		media({ minWidth: 701 }, { fontSize: '60px' }),
+	)
 
 ReactDOM.render(
 	<div className={className}>
